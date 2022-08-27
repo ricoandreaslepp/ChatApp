9bea7ac4980f59000fc01e27bfe0e4e9e5aebaa1
ricoandreaslepp <ricoandreaslepp@gmail.com>
2022-08-27 20:54:20 +0300

    moved Client class

--- server.py
+++ server.py
@@ -2,11 +2,66 @@
 # import uuid; uuid = str(uuid.uuid4())
 # import colorama
 
+from threading import Thread
 from config import HOST, PORT
-from client import Client
 from logger import log
 import socket
 
+class Client(Thread):
+
+    def __init__(self, conn, addr, name):
+        super().__init__()
+        self.conn = conn
+        self.addr = addr
+        self.name = name  # currently used as the only identifier
+
+    # 1) send client closing information
+    # 2) remove client from Server.__connections
+    # 3) log that client exited
+    def disconnect(self):
+        try:
+            self.conn.send("\n\nServer closing connection...".encode())
+        except BrokenPipeError:
+            pass
+
+        self.conn.close()
+
+        Server.remove_connection(self)
+        log.info(f"{self.name} {self.addr} closed connection")
+
+    # check for messages while you were logged out
+    def check_messages(self):
+        pass
+
+    # Thread method, that runs itself
+    def run(self):
+        self.conn.send("Successfully connected to server...\n\n".encode())
+        Server.motd(self.conn)
+
+        try:
+            while True:
+                self.conn.send(f"[{self.name}]: ".encode())
+                data = self.conn.recv(1024).decode().strip()
+
+                if data == "":
+                    break
+
+                # log names for localhost testing
+                # log.data_transfer([self.name, "server", data])
+
+                # do something
+                for client in Server.connections():
+                    if client.name != self.name:
+                        client.conn.send(f"\r[{self.name}]: {data}\n".encode())
+                        client.conn.send(f"[{client.name}]: ".encode())  # restore their screen
+                        log.data_transfer([client.name, "server", data])
+
+        except BrokenPipeError:
+            pass
+        except Exception as e:
+            log.info(e)
+
+        self.disconnect()
 
 class Server:
 
